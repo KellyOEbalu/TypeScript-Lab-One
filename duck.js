35428d78df63403893dc1e1ee5e6c595deee219b
Kelly_Ebalu <KellyO.Ebalu@Gmail.com>
2022-02-27 13:23:44 +0000

    lab one upload

---
+++ duck.js
@@ -0,0 +1,7 @@
+function addition(value) {
+    console.log("Value is: " + value);
+}
+var firstVal = 5;
+var secondVal = 5;
+var sumOfVals = (firstVal + secondVal).toLocaleString();
+addition(sumOfVals);
---
+++ duck.ts
@@ -0,0 +1,10 @@
+function addition(value: string)
+{
+    console.log("Value is: " + value);
+}
+    let firstVal: number = 5;
+    let secondVal: number = 5;
+    let sumOfVals: string = (firstVal + secondVal).toLocaleString();
+    
+    addition(sumOfVals);
+    
